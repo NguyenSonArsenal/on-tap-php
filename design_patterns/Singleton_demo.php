4f87e42dda90036d4c235b55f445e88863f65c2e
son <vanson297.nguyen@gmail.com>
2017-12-02 17:00:48 +0700

    add signleton demo

---
+++ design_patterns/Singleton_demo.php
@@ -0,0 +1,27 @@
+<?php
+
+class Singleton_demo
+{
+    // Only a element of this class is exit
+    private static $instance = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if(null === self::$instance)
+        {
+            self::$instance = new Singleton_demo;
+            echo 'New';
+            return new Singleton_demo;
+        }
+        echo 'Olds';
+        return static::$instance;
+    }
+}
+
+$singleton = Singleton_demo::getInstance();
+$singleton = Singleton_demo::getInstance();
+$singleton = Singleton_demo::getInstance();
