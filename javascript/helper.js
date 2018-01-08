7241fec19a2869e60eb67510c112ff5555f7ea92
NguyenSonArsenal <vanson297.nguyen@gmail.com>
2018-01-08 17:33:24 +0700

    + add exercise string javascript

--- javascript/helper.js
+++ javascript/helper.js
@@ -18,3 +18,20 @@ isNumber = (n) => {
     }
     return false;
 };
+
+function lcfirst(str) {
+    var str = str.toString().trim();
+
+    var result = str.charAt(0).toLowerCase() + str.slice(1);
+
+    return result;
+}
+
+function ucfirst(str) {
+    var str = str.toString().trim();
+
+    var result = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return result;
+}
+
